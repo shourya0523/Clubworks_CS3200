d406b23de78b550d26a3934323f268954b472b68
riyaramdev <ramdev.r@northeastern.edu>
2025-04-13 17:03:06 -0400

    Testing out some streamlit functions

--- app/src/Home.py
+++ app/src/Home.py
@@ -73,5 +73,10 @@ if st.button('Act as System Administrator',
     st.session_state['first_name'] = 'SysAdmin'
     st.switch_page('pages/20_Admin_Home.py')
 
-
-
+if st.button('Act as systems coordinator trial', 
+            type = 'primary', 
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'coordinator'
+    st.session_state['first_name'] = 'Connor'
+    st.switch_page('pages/streamlit_trial.py')
